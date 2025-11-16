c21f81e104251f466cb39f687e7c793e4c9537c3
iisalut <tulasiin@gmail.com>
2025-11-16 07:32:18 -0500

    test

--- area_2d_2.gd
+++ area_2d_2.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var entered= false
+
 			
 func _on_body_entered(body: PhysicsBody2D) -> void:
 	entered=true
@@ -8,8 +9,22 @@ func _on_body_entered(body: PhysicsBody2D) -> void:
 
 func _on_body_exited(body: Node2D) -> void:
 	entered=false
-
-func _process(delta):
-	if entered== true :
+	if entered == true:
 		if Input.is_action_just_pressed("ui_accept"):
-			get_tree().change_scene_to_file("res://boss_mini_game.tscn")
+
+			var highest = max(guilt, superiority, inferiority, persecution, martyr)
+			
+			if highest == superiority:
+				get_tree().change_scene_to_file("res://boss_mini_game.tscn")
+
+			elif highest == inferiority:
+				get_tree().change_scene_to_file("res://inferiority_minigame.tscn")
+
+			elif highest == guilt:
+				get_tree().change_scene_to_file("res://guilt_minigame.tscn")
+
+			elif highest == persecution:
+				get_tree().change_scene_to_file("res://persecution_minigame.tscn")
+
+			elif highest == martyr:
+				get_tree().change_scene_to_file("res://martyr_minigame.tscn")
