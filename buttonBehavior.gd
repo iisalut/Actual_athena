96f7647fda0fcbec16266bf554309a8ea41810b8
Avishi Sharma <robloxpusheen8@gmail.com>
2025-11-16 00:38:46 -0500

    Game logic-- updating UI based on question, variable assignment

--- buttonBehavior.gd
+++ buttonBehavior.gd
@@ -1,11 +1,10 @@
 extends Button
 
+var complex
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var button = Button.new()
-	button.text = "option 1"
-	button.pressed.connect(_button_pressed)
-	add_child(button)
+	pass
 
-func _button_pressed():
-	print("hi")
+func _on_pressed() -> void:
+	print(complex)
